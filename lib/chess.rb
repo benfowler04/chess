6525fc3286ef5268eb69332b9943c2a04fd3caff
Ben Fowler <46171044+benfowler04@users.noreply.github.com>
2019-12-13 13:14:05 -0500

    completed project

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,17 +1,32 @@
 require './lib/board'
 require './lib/player'
+require 'yaml'
 
 class Chess
-    def initialize(player1, player2)
-        @players = []
-        @players.push(Player.new(player1, "white"))
-        @players.push(Player.new(player2, "black"))
-        @board = Board.new
+    def initialize(player1, player2, load_game = false)
+        unless load_game
+            @players = []
+            @players.push(Player.new(player1, "white"))
+            @players.push(Player.new(player2, "black"))
+            @board = Board.new
+        end
+        if load_game
+            load
+        end
+    end
+
+    def load
+        save_data = YAML.load(File.read("chess.yml"))
+        @board = save_data[:board]
+        @players = save_data[:players]
+        @current_player = save_data[:current]
+        @next_player = save_data[:next]
+        @loaded_game = true
     end
 
     def play
-        @current_player = @players.first
-        @next_player = @players.last
+        @current_player = @players.first unless @loaded_game
+        @next_player = @players.last unless @loaded_game
         game_over = false
         until game_over
             piece_to_move = false
@@ -22,7 +37,8 @@ class Chess
                 puts
                 puts "It's #{@current_player.name}'s turn. Select a piece to move."
                 puts "Columns are 1-8 (starting at left), rows are 1-8 (starting at bottom)."
-                puts "Enter column & row of piece to move (no space), 'c' to castle, or 'r' to resign:"
+                puts "Enter column & row of piece to move (with no space in between)."
+                puts "Or, enter 'c' to castle, 'r' to resign, or 's' to save:"
                 start_position = gets.chomp
                 next if start_position.empty?
                 if "rR".include?(start_position)
@@ -32,6 +48,11 @@ class Chess
                 castling = true if "cC".include?(start_position)
                 next puts "You cannot castle." if castling unless @board.can_player_castle?(@current_player.piece_color, "q") || @board.can_player_castle?(@current_player.piece_color, "k")
                 break if castling
+                if "sS".include?(start_position)
+                    save
+                    puts "Game saved."
+                    exit
+                end
                 piece_to_move = @board.is_valid_start?(start_position, @current_player.piece_color)
             end
             valid_move = false
@@ -76,5 +97,14 @@ class Chess
                 @current_player, @next_player = @next_player, @current_player
             end
         end
+    end
+
+    private
+    def save
+        save_data = { board: @board,
+                    players: @players,
+                    current: @current_player,
+                    next: @next_player }
+        save_file = File.open("chess.yml","w") { |file| file.write(save_data.to_yaml) }
     end
 end
--- main.rb
+++ main.rb
@@ -1,21 +1,39 @@
 require './lib/chess'
 
+valid_input = false
+load_game = false
+
+if File.exist?("chess.yml")
+    until valid_input
+        puts "(S)tart a new game or (l)oad a saved game?"
+        choice = gets.chomp
+        next if choice.empty?
+        next unless "sSlL".include?(choice)
+        valid_input = true
+        load_game = true if "lL".include?(choice)
+        puts
+    end
+end
+
 name_entered_1 = false
 name_entered_2 = false
 
-until name_entered_1
-    puts "Player 1, what's your name?"
-    player1 = gets.chomp
-    next if player1.empty?
-    name_entered_1 = true
-    puts
-end
-until name_entered_2
-    puts "Player 2, what's your name?"
-    player2 = gets.chomp
-    next if player2.empty?
-    name_entered_2 = true
+unless load_game
+    until name_entered_1
+        puts "Player 1, what's your name?"
+        player1 = gets.chomp
+        next if player1.empty?
+        name_entered_1 = true
+        puts
+    end
+    until name_entered_2
+        puts "Player 2, what's your name?"
+        player2 = gets.chomp
+        next if player2.empty?
+        name_entered_2 = true
+    end
 end
 
-game = Chess.new(player1, player2)
+game = Chess.new(player1, player2) unless load_game
+game = Chess.new("player1", "player2", true) if load_game
 game.play
